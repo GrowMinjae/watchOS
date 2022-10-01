dcfb6a8411a8e1bbc1a27f2cf4f4f16ffde9ce0b
minaje0917 <k050918@naver.com>
2022-10-01 15:13:12 +0900

    Initial Commit

---
+++ WatchOS Watch App/WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSApp.swift
+//  WatchOS Watch App
+//
+//  Created by 선민재 on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
